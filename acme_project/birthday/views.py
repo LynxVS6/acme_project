3380f8d4b385711ec7c433d2db94ce54dab5a00d
Рысьева Виктория <Viktoriariseva@ya.ru>
2024-12-20 02:05:38 +0500

    Add mixins

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,38 +1,33 @@
-from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.urls import reverse_lazy
 
 from .models import Birthday
 from .forms import BirthdayForm
-from .utils import calculate_birthday_countdown
 
 
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    else:
-        instance = None
-    form = BirthdayForm(request.POST or None, instance=instance)
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_date = form.cleaned_data['birthday']
-        birthday_countdown = calculate_birthday_countdown(
-            birthday_date
-        )
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayMixin:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
 
 
-def delete_birthday(request, pk):
-    instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
 
 
-def birthday_list(request):
-    birthdays = Birthday.objects.all()
-    context = {'birthdays': birthdays}
-    return render(request, 'birthday/birthday_list.html', context)
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    pass
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 10

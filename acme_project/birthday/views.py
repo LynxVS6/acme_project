2db75df87a42a7588edb3cf6272fd6879009ed63
Рысьева Виктория <Viktoriariseva@ya.ru>
2024-12-19 23:32:59 +0500

    Add birthday list

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from .models import Birthday
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 
@@ -8,8 +9,15 @@ def birthday(request):
     form = BirthdayForm(request.POST or None)
     context = {'form': form}
     if form.is_valid():
+        form.save()
         birthday_countdown = calculate_birthday_countdown(
             form.cleaned_data['birthday']
         )
         context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    birthdays = Birthday.objects.all()
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)

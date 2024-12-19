8888f1ca99d297ec47d047e618b3ad23853e862d
Рысьева Виктория <Viktoriariseva@ya.ru>
2024-12-20 00:46:47 +0500

    Edit and delete

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,19 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Birthday
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 
 
-def birthday(request):
-    form = BirthdayForm(request.POST or None)
+def birthday(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    form = BirthdayForm(request.POST or None, instance=instance)
     context = {'form': form}
     if form.is_valid():
         form.save()
+        birthday_date = form.cleaned_data['birthday']
         birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
+            birthday_date
         )
         context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
+
+
+def delete_birthday(request, pk):
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('birthday:list')
     return render(request, 'birthday/birthday.html', context)
 
 
